b5efb76fb276b52b3b5877d05da23ee248d01e7b
Produlx <dulsathlenovo12@gmail.com>
2025-05-12 18:29:47 +0530

    added film

--- Variables.py
+++ Variables.py
@@ -76,6 +76,7 @@ MEG_2 = 1
 DEAD_RACING_3 = 2
 THE_LOST = 3 
 SUN_FUN_RUN = 4
+BLACK_panther = 5
 
 print("")
 print(f'We have avalible to watch {MEG_2} , {DEAD_RACING_3} , {THE_LOST} , {SUN_FUN_RUN}')
